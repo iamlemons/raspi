37306a97118ab56f6fe58be513707540f3eb5a2a
iamlemons <josh.maccabee@gmail.com>
2016-08-02 16:11:37 -0400

    Cleanup
    * Moves the core package maintenance logic to `maintain_pi.sh`
    * Adds exit status of 1 to user not being root
    * Make setup file names variables

--- rpi-config/setup_pi.sh
+++ rpi-config/setup_pi.sh
@@ -1,54 +1,47 @@
 #!/bin/bash
-# Script to setup a Raspberry Pi 
+# Script to setup a Raspberry Pi for the first time
+
+# set paths to our configuration files
+$RPI_MAINTENANCE_FILE=maintain_pi.sh
+$RPI_WIFI_SETUP_FILE=setup_wifi.sh
 
 # first, verify that the user is root
 if [[ $EUID -ne 0 ]]; then 
     echo "Must be run as root!"
-    exit
+    exit 1
 fi
 
-# first, we'll update our `apt-get` 
-apt-get update
-apt-get upgrade
+# first, we'll update our key `apt-get`, `easy_install`, and `Python` libraries
+# to the latest distributions using what will then become our maintenance script
+if [[ ! -f ./$RPI_MAINTENANCE_FILE ]]; then
+    echo "RPi maintenance file $RPI_MAINTENANCE_FILE missing... Try recloning the repo."
+    exit 1
+else
+    bash ./$RPI_MAINTENANCE_FILE
+fi
 
 # next, so that we can easily edit our files, install and configure vim
 apt-get install vim 
 update-alternatives --config editor
 
-# next, we'll update `easy_install`
-easy_install -U distribute
-
-# then we'll update our Python libraries to ensure they're the latest
-apt-get install python
-apt-get install python-setuptools
-apt-get install python-pip
-
-# and update the RPi Python libraries
-pip install RPi.GPIO
-pip install pySerial
-pip install nose
-
-# and we'll give ourselves a better Python environment to test in
-pip install ipython
-
 # next we change the keyboard configuration
+# NOTE: we will want to select the Generic US keyboard layout
 apt-get install console-common
 dpkg-reconfigure keyboard-configuration
 
-# NOTE: we will want to select the Generic US keyboard layout
 # once that's done, change the password to user Pi for added security
 passwd
 
 # prompt the user if they want to also setup Wi-Fi
-read -r -p "Do you want to configure Wi-Fi access? [y/N] >>> " WIFI_RESP
+read -prompt "Do you want to configure Wi-Fi access? [y/N] >>> " WIFI_RESP
 if [[ $WIFI_RESP =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    if [[ ! -f ./setup_wifi.sh ]]; then
-        echo "Wi-Fi setup file missing... Rebooting."
+    if [[ ! -f ./$RPI_WIFI_SETUP_FILE ]]; then
+        echo "Wi-Fi setup file $RPI_WIFI_SETUP_FILE missing... Aborting Wi-Fi setup."
     else
-        bash ./setup_wifi.sh
+        bash ./$RPI_WIFI_SETUP_FILE
     fi
 fi
 
 # to persist the changes we just made, reboot now
-echo "Setup succeeded! Rebooting to persist changes..."
+echo "Setup complete! Rebooting to persist changes..."
 reboot
