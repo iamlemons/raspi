19dd76230013a727e1d7733455f989332b92166d
iamlemons <josh.maccabee@gmail.com>
2016-08-02 16:46:01 -0400

    Add exit code to setup_wifi.sh
    Minor tweaks to improve `setup_wifi.sh`

--- rpi-config/setup_wifi.sh
+++ rpi-config/setup_wifi.sh
@@ -5,18 +5,22 @@
 # first, confirm that we're running as root
 if [[ "$EUID" -ne 0 ]]; then
     echo "Must be run as root!"
-    exit
+    exit 1
 fi
 
-read -r -p "Enter an SSID >>> " SSID
+# set the path to WPA_SUPPLICANT_FILE
+WPA_SUPPLICANT_FILE=/etc/wpa_supplicant/wpa_supplicant.conf
+
+read -prompt "Enter an SSID >>> " SSID
 
 # check that the given SSID exists
 if [[ -z "$SSID" ]]; then
     echo "No SSID provided... Aborting Wi-Fi config and rebooting."
+    exit 1
 elif [[ $(sudo iw dev wlan0 scan | grep "$SSID") ]]; then
-    read -r -p "Enter the password for the SSID >>> " SSID_PASSWD
+    read -prompt "Enter the password for $SSID >>> " SSID_PASSWD
     if [[ ! -z "$SSID_PASSWD" ]]; then
-        WPA_SUPPLICANT_FILE=/etc/wpa_supplicant/wpa_supplicant.conf
+        # add the network information to the $WPA_SUPPLICANT_FILE
         echo "" >> $WPA_SUPPLICANT_FILE
         echo "network={" >>  $WPA_SUPPLICANT_FILE
         echo "    ssid=" $SSID >> $WPA_SUPPLICANT_FILE 
@@ -25,9 +29,11 @@ elif [[ $(sudo iw dev wlan0 scan | grep "$SSID") ]]; then
         echo "" >> $WPA_SUPPLICANT_FILE
     else 
         echo "No password provided... Aborting Wi-Fi config and rebooting."
-    fi 
+        exit 1
+    fi
 else 
      echo "SSID not found... Aborting Wi-Fi config and rebooting." 
+     exit 1
 fi 
 
-exit
+exit 0
